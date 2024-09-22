b764f986aecb88c4df8756b380921bbe37bd56c9
Joowww <72143824+Joowww@users.noreply.github.com>
2024-09-22 11:42:30 +0200

    Add files via upload

---
+++ Proyecto2_PGTA/AstDecoder/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MultiCAT6.Utils;
+
+namespace AstDecoder
+{
+    public class CAT048
+    {
+        public string SAC { get; set; }
+        public string SIC { get; set; }
+    }
+}
