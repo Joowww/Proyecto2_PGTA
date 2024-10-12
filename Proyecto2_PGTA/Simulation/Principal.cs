acec10b2e9bb098d0ccc247d14c4fb4e98ad9618
mireiaviladots <mireiaviladot@gmail.com>
2024-10-12 13:22:11 +0200

    WF 1

---
+++ Proyecto2_PGTA/Simulation/Principal.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Simulate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
